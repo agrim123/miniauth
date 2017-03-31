9df6d769db590b0df26f56bdae93db6558b2d645
hitman <agrimmittal97@gmail.com>
2017-04-01 00:15:46 +0530

    set views dir in server

--- server.js
+++ server.js
@@ -1,18 +1,17 @@
 // server.js
 
-// set up ======================================================================
-// get all the tools we need
 var express  = require('express');
 var app      = express();
 var port     = process.env.PORT || 3000;
 var mongoose = require('mongoose');
 var passport = require('passport');
 var flash    = require('connect-flash');
-
+var path     = require('path');
 var morgan       = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser   = require('body-parser');
 var session      = require('express-session');
+require('dotenv').config();
 
 var configDB = require('./config/database.js');
 
@@ -27,11 +26,11 @@ app.use(cookieParser()); // read cookies (needed for auth)
 app.use(bodyParser.json()); // get information from html forms
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.set('view engine', 'ejs'); // set up ejs for templating
+app.set('views',path.join(__dirname, 'app/views')); // set up ejs for templating
 
 // required for passport
 app.use(session({
-    secret: 'secret', // session secret
+    secret: process.env.SESSION_SECRET, // session secret
     resave: true,
     saveUninitialized: true
 }));
@@ -40,7 +39,7 @@ app.use(passport.session()); // persistent login sessions
 app.use(flash()); // use connect-flash for flash messages stored in session
 
 // routes ======================================================================
-require('./app/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
+require('./routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
 
 // launch ======================================================================
 app.listen(port);
